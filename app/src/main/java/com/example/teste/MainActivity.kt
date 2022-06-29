44010915979c2d6e0a90c379449a3facbec5590d
Deborah Heinig <deborahheinig@hotmail.com>
2022-06-28 22:23:05 -0300

    new button commit

--- app/src/main/java/com/example/teste/MainActivity.kt
+++ app/src/main/java/com/example/teste/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.teste
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val tvMyTextView = findViewById<TextView>(R.id.textView)
+        btnClickMe.setOnClickListener {
+            btnClickMe.text = "You clicked me!"
+            tvMyTextView.text = "You clicked him!"
+        }
+
     }
 }
